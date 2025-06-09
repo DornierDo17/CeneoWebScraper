f890b9671cc8ca8c9699d05058b8781211df2716
dornierdo17 <andriikol17@gmail.com>
2025-06-09 15:29:51 +0200

    09-06-2025-KA

--- app/models.py
+++ app/models.py
@@ -1,12 +1,11 @@
-from app.utils import extract_data, translate_data
-from config import headers
-import requests
-import numpy as np
+import os
+import json
 import pandas as pd
-import  json, os
+import numpy as np
+import requests
 from bs4 import BeautifulSoup
-
-
+from config import headers
+from app.utils import extract_data, translate_data
 
 class Product:
     def __init__(self, product_id, product_name="", opinions=[], stats={}):
@@ -16,22 +15,21 @@ class Product:
         self.stats = stats
 
     def __str__(self):
-        return f'product_id: {self.product_id}\nProduct_name:{self.product_name}\nOpinions: '+"\n\n".join([str(opinion) for opinion in self.opinions])+"\n"+json.dumps(self.stats, indent=4, ensure_ascii=False)+"\n"
-
-
+        return f"Product_id: {self.product_id}\nProduct_name: {self.product_name}\nOpinions: "+"\n\n".join([str(opinion) for opinion in self.opinions])+"\n"+json.dumps(self.stats, indent=4, ensure_ascii=False)+"\n"
+    
     def __repr__(self):
         return f"Product(product_id={self.product_id}, product_name={self.product_name}, opinions=["+",".join(repr(opinion) for opinion in self.opinions)+f"], stats={self.stats})"
-    
+
     def extract_name(self):
         next_page = f"https://www.ceneo.pl/{self.product_id}#tab=reviews"
         response = requests.get(next_page, headers = headers)
         if response.status_code == 200:
             page_dom = BeautifulSoup(response.text, 'html.parser')
             self.product_name = extract_data(page_dom, "h1")
-            opinions_count = extract_data(page_dom, "product-review__link > span") 
-            return True if opinions_count else False
+            opinions_count = extract_data(page_dom, "a.product-review__link > span")
+            return bool(opinions_count)
         else:
-            return False 
+            return False
 
     def extract_opinions(self):
         next_page = f"https://www.ceneo.pl/{self.product_id}#tab=reviews"
@@ -44,13 +42,10 @@ class Product:
                 print(len(opinions))
                 for opinion in opinions:
                     single_opinion = Opinion()
-                    single_opinion.extract(opinion)
-                    single_opinion.translate()
-                    single_opinion.transform()
+                    single_opinion.extract(opinion).translate().transform()
                     self.opinions.append(single_opinion)
-
                 try:
-                    next_page = "https://www.ceneo.pl" + extract_data(page_dom, "a.pagination__next", "href")
+                    next_page = "https://www.ceneo.pl" + extract_data(page_dom,"a.pagination__next","href")
                 except TypeError:
                     next_page = None
 
@@ -65,67 +60,65 @@ class Product:
     def export_info(self):
         if not os.path.exists("./app/data"):
             os.mkdir("./app/data")
-        if not os.path.exists("./app/data/opinions"):
-            os.mkdir("./app/data/opinions")
-
-        with open(f"./app/data/opinions/{self.product_id}.json", "w", encoding="UTF-8") as jf:
+        if not os.path.exists("./app/data/products"):
+            os.mkdir("./app/data/products")
+        with open(f"./app/data/products/{self.product_id}.json", "w", encoding="UTF-8") as jf:
             json.dump(self.transform_to_dict(), jf, ensure_ascii=False, indent=4)
 
     def transform_to_dict(self):
-        return {'prodcut_id': self.product_id,
-                'product_name': self.product_name,
-                'stats': self.stats}
-
+        return {
+            'product_id': self.product_id,
+            'product_name': self.product_name,
+            'stats': self.stats
+        }
+    
     def import_opinions(self):
-        with open(f"./app/data/opinions/{self.product_id}.json", "r", encoding="UTF-8") as jf:
-                opinions = json.load(jf)
-
+        with open(f".app/data/opinions/{self.product_id}.json", "r", encoding="UTF-8") as jf:
+            opinions = json.load(jf)
         for opinion in opinions:
             single_opinion = Opinion()
-            for key, value in opinion.item():
+            for key, value in opinion.items():
                 setattr(single_opinion, key, value)
-            self.opinions.append(single_opinion) 
+            self.opinions.append(single_opinion)
 
     def import_info(self):
-        with open(f"./app/data/opinions/{self.product_id}.json", "r", encoding="UTF-7") as jf:
-            info=json.load(jf)
-        self.product_name = info["product_name"]
-        self.stats = info["stats"]
-
-
-
+        with open(f".app/data/products/{self.product_id}.json", "r", encoding="UTF-8") as jf:
+            info = json.load(jf)
+        self.product_name = info['product_name']
+        self.stats = info['stats']
 
     def analyze(self):
-        opinions = pd.DataFrame.from_dict([opinion.transform_to_dict() for opinion in self.opinions]) 
+        opinions = pd.DataFrame.from_dict([opinion.transform_to_dict() for opinion in self.opinions])
         self.stats["opinions_count"] = opinions.shape[0]
-        self.stats["average_rate"]  = round(opinions["stars"].mean(), 1)
-        self.stats["pros_count"]  = opinions["pros"].astype(bool).sum()
-        self.stats["cons_count"]  = opinions["cons"].astype(bool).sum()
-        self.stats["pros_cons_count"]  = opinions.apply(lambda x: bool(x["pros"]) and bool(x["cons"]), axis=1).sum()
-        self.stats["pros"] = opinions["pros_en"].explode().value_counts()
-        self.stats["cons"] = opinions["cons_en"].explode().value_counts()
-        self.stats["recommendations"] = opinions["recommendation"].value_counts(dropna=False)
-        self.stats["stars"] = opinions["stars"].value_counts().reindex(list(np.arange(0, 5, 0.5)), fill_value=0).sort_index()
-
+        self.stats["pros_count"] = int(opinions.pros_pl.astype(bool).sum())
+        self.stats["cons_count"] = int(opinions.cons_pl.astype(bool).sum())
+        self.stats["pros_cons_count"] = int(opinions.apply(lambda o: bool(o.pros_pl) and bool(o.cons_pl), axis=1).sum())
+        self.stats["average_rate"] = float(opinions.stars.mean())
+        self.stats["pros"] = opinions.pros_en.explode().value_counts().to_dict()
+        self.stats["cons"] = opinions.cons_en.explode().value_counts().to_dict()
+        self.stats["recommendations"] = opinions.recommendation.value_counts(dropna=False).reindex([False, True, np.nan], fill_value=0).to_dict()
+        self.stats["stars"] = opinions.stars.value_counts().reindex(list(np.arange(0,5.5,0.5)), fill_value=0).to_dict()
 
 class Opinion:
-
     selectors = {
-            "opinion_id" :  (None, "data-entry-id"),
-            "author" : ("span.user-post__author-name", ),
-            "recommendation" : ("span.user-post__author-recomendation > em",),
-            "stars" : ("span.user-post__score-count",),
-            "content_pl" : ("div.user-post__text",),
-            "pros_pl" : ("div.review-feature__item--positive", None, True),
-            "cons_pl" : ("div.review-feature__item--negative", None, True),
-            "vote_yes" : ("button.vote-yes","data-total-vote"),
-            "vote_no" : ("button.vote-no","data-total-vote"),
-            "published" : ("span.user-post__published > time:nth-child(1)","datetime"),
-            "purchased" : ("span.user-post__published > time:nth-child(2)","datetime")
+        "opinion_id": (None, "data-entry-id"),
+        "author": ("span.user-post__author-name",),
+        "recommendation": ("span.user-post__author-recomendation > em",),
+        "stars": ("span.user-post__score-count",),
+        "content_pl": ("div.user-post__text",),
+        "pros_pl": ("div.review-feature__item--positive", None, True),
+        "cons_pl": ("div.review-feature__item--negative", None, True),
+        "vote_yes": ("button.vote-yes","data-total-vote"),
+        "vote_no": ("button.vote-no","data-total-vote"),
+        "published": ("span.user-post__published > time:nth-child(1)","datetime"),
+        "purchased": ("span.user-post__published > time:nth-child(2)","datetime"),
+        "content_en": (),
+        "pros_en": (),
+        "cons_en": ()
     }
 
-
-    def __init__(self, opinion_id="", author="", recommendation=False, stars=0.0, content="", pros=[], cons=[], vote_yes=0, vote_no=0, published_date="", purchased_date="") :
+    def __init__(self, opinion_id="", author="", recommendation=False, stars=0.0, content="", pros=[], cons=[], vote_yes=0, vote_no=0, publish_date="", purchase_date=""):
+        self.opinion_id = opinion_id
         self.author = author
         self.recommendation = recommendation
         self.stars = stars
@@ -134,37 +127,37 @@ class Opinion:
         self.cons_pl = cons
         self.vote_yes = vote_yes
         self.vote_no = vote_no
-        self.published_date = published_date
-        self.purchased_date = purchased_date
-        self.opinion_id = opinion_id
+        self.publish_date = publish_date
+        self.purchase_date = purchase_date
         self.content_en = ""
         self.pros_en = []
         self.cons_en = []
-        
+    
     def __str__(self):
-        return "\n".join([f"{key}: {getattr(self, key)}" for key in self.selectors.keys()]) # could be wrong, check later
+        return ("\n".join([f"{key}: {getattr(self, key)}" for key in self.selectors.keys()]))
     
     def __repr__(self):
-        return f"Opinion(\n" + "\n".join([f"{key}: {getattr(self, key)}" for key in self.selectors.keys()]) + "\n)"
+        return "Opinion("+", ".join([f"{key}={str(getattr(self, key))}" for key in self.selectors.keys()])+")"
     
-    def tranforms_to_dict(self):
-        return {key: getattr(self, key) for key in self.selectors.keys()} 
-    
+    def extract(self, opinion_tag):
+        for key, value in self.selectors.items():
+            setattr(self, key, extract_data(opinion_tag, *value))
+        return self
+
     def translate(self):
         self.content_en = translate_data(self.content_pl)
         self.pros_en = [translate_data(pros) for pros in self.pros_pl]
         self.cons_en = [translate_data(cons) for cons in self.cons_pl]
+        print(self)
         return self
 
     def transform(self):
-        self.recommendation = True if self.recommendation == "Polecam" else False if self.recommendation == "Nie polecam" else None
-        self.starts = float(self.stars.split("/")[0].replace(",", "."))
+        self.recommendation = True if self.recommendation=='Polecam' else False if  self.recommendation=="Nie polecam" else None
+        self.stars = float(self.stars.split("/")[0].replace(",", "."))
         self.vote_yes = int(self.vote_yes)
         self.vote_no = int(self.vote_no)
         return self
     
-    def extract(self, opinion):
-        for key, value in self.selectors.items():
-            setattr(self, key, extract_data(opinion, *value))
-        return self
-
+    def transform_to_dict(self):
+        return {key: getattr(self, key) for key in self.selectors.keys()}
+    
--- app/views.py
+++ app/views.py
@@ -1,48 +1,57 @@
-from app.models import Product
 from app import app
 from flask import render_template, redirect, url_for, request
 from app.forms import ExtractForm
+from app.models import Product
+import json
 
-
-@app.route('/')    
+@app.route("/")
 def index():
     return render_template("index.html")
 
-
-@app.route('/extract')    
+@app.route("/extract")
 def render_form():
     form = ExtractForm()
     return render_template("extract.html", form=form)
 
-
-@app.route('/extract', methods=['POST'])    
+@app.route("/extract", methods=['POST'])
 def extract():
     form = ExtractForm(request.form)
     if form.validate():
         product_id = form.product_id.data
         product = Product(product_id)
         if product.extract_name():
-            product.export_opinions()
+            product.extract_opinions()
             product.analyze()
             product.export_info()
             product.export_opinions()
-            
-            return redirect(url_for("product", product_id=product_id))
-        form.product_id.errors.append("The is no product for provided id or product has no opinions")
+            return redirect(url_for('product', product_id=product_id))
+        form.product_id.errors.append('There is no product for provided id or product has no opinions')
         return render_template('extract.html', form=form)
-    return render_template("extract.html", form=form)
+    return render_template('extract.html', form=form)
 
-
-@app.route('/products')    
+@app.route("/products")
 def products():
-    return render_template("products.html")
+    products = []
+    try:
+        with open("./app/data/products", "r", encoding="UTF-8") as jf:
+            products = json.load(jf)
+    except FileNotFoundError:
+        pass
+    except json.JSONDecodeError:
+        pass
+    if not products:
+        return render_template("products.html", product=None)
+    products = [Product(product['product_id']) for product in products]
+    for product in products:
+        product.import_info()
+        product.import_opinions()
 
+    return render_template("products.html", product=products)
 
-@app.route('/product/<product_id>')    
+@app.route("/product/<product_id>")
 def product(product_id):
     return render_template("product.html", product_id=product_id)
 
-
-@app.route('/about')    
+@app.route("/about")
 def about():
     return render_template("about.html")
